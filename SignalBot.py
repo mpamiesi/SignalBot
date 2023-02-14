001ac6ee3d1c9bf6c6b4098403ac965282f5a4f0
mpamiesi <113771379+mpamiesi@users.noreply.github.com>
2023-02-14 14:53:21 +0100

    Create SignalBot.py

---
+++ SignalBot.py
@@ -0,0 +1,22 @@
+import telegram
+
+def send_signal(trading_pair, signal_type):
+    bot_token = "6119983782:AAGwSeqDU2E7UfyhIz6t2wh81AGopsMjZx8"
+    bot = telegram.Bot(token=bot_token)
+    channel_name = "@SignalBot100"
+    bot.send_message(chat_id=channel_name, text=trading_pair + ": " + signal_type)
+
+def main(trading_pair, signal_text):
+    if signal_text == "Long1" or signal_text == "Long2":
+        send_signal(trading_pair, "Long Signal")
+    elif signal_text == "Venta L1" or signal_text == "Venta L2":
+        send_signal(trading_pair, "Close Long Signal")
+    elif signal_text == "Short1" or signal_text == "Short2":
+        send_signal(trading_pair, "Short Signal")
+    elif signal_text == "Cierre S1" or signal_text == "Cierre S2":
+        send_signal(trading_pair, "Close Short Signal")
+
+if __name__ == "__main__":
+    trading_pair = input("Enter the trading pair: ")
+    signal_text = input("Enter the signal text: ")
+    main(trading_pair, signal_text)
